ddef32fe23fcc174e97c1f9fc8976e36217d6f34
Mark Provan <mrp4@st-andrews.ac.uk>
2016-09-22 18:44:46 +0100

    Change app.style to SCSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { AppState } from './app.service';
   selector: 'app',
   encapsulation: ViewEncapsulation.None,
   styleUrls: [
-    './app.style.css'
+    './app.style.scss'
   ],
   template: `
     <md-content>
