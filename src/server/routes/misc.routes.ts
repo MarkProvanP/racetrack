36f0e3c36432c1ba8b6336ee878ab7843e23d07c
Mark Provan <markprovanp@gmail.com>
2017-01-13 14:17:39 +0000

    Force browser to cache map pins

--- src/server/routes/misc.routes.ts
+++ src/server/routes/misc.routes.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as winston from "winston";
+import * as moment from "moment";
 
 import { Team } from "../../common/team";
 import { NotFoundError } from "../errors";
@@ -33,6 +34,11 @@ export default function miscRouterWithDb(dataIntermediate: DataIntermediary) {
     let teamId = req.params.id;
     return dataIntermediate.getTeamMapPinSVG(teamId)
     .then(svg => {
+      const CACHE_TIME_MOMENT = moment.duration({
+        hours: 1
+      })
+      res.setHeader("Cache-Control", `public, max-age=${CACHE_TIME_MOMENT.asSeconds()}`)
+      res.setHeader("Expires", moment().add(CACHE_TIME_MOMENT).toDate().toUTCString());
       res.type("image/svg+xml");
       res.send(svg);
     })
